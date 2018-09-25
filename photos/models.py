43ce0cbf4d8ca3491a5f689d568f37f58dfc729e
IreriVikki <wambsviki@gmail.com>
2018-09-25 19:34:36 +0300

    added a location model to the models

--- photos/models.py
+++ photos/models.py
@@ -10,19 +10,19 @@ class Location(models.Model):
         return self.location
 
 
-class Category(models.Model):
-    name = models.CharField(max_length=30)
+# class Category(models.Model):
+#     name = models.CharField(max_length=30)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+# class Image(models.Model):
+#     image = models.ImageField(upload_to='images/')
+#     name = models.CharField(max_length=50)
+#     description = models.TextField()
+#     location = models.ForeignKey(Location)
+#     category = models.ForeignKey(Category)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+# from django.test import TestCase
+# from .models import Image, Category, Location
 
-# Create your tests here.
+# # Create your tests here.
+
+
+# class GaleryTestCase(TestCase):
+
+# def setUp(self):
+#     self.nairobi = Location(locatio='Nairobi')
+#     self.nairobi.save()
+#     self.nature = Category(name='nature')
+#     self.nature.save()
+
+#     self.waterfall = Image(
+#         name='waterfall', description='picture of a waterfall')
+
+#     self.waterfall.category.add(self.nature)
+#     self.waterfall.location.add(self.nairobi)
+#     self.waterfall.save()
