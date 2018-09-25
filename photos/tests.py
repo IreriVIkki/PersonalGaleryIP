d6462c448e42fa213598176d0338a8923d8e22de
IreriVikki <wambsviki@gmail.com>
2018-09-25 19:35:34 +0300

    added a test class for the project and created the setup test instance

--- photos/tests.py
+++ photos/tests.py
@@ -1,20 +1,21 @@
-# from django.test import TestCase
-# from .models import Image, Category, Location
+from django.test import TestCase
+from .models import Image, Category, Location
 
-# # Create your tests here.
+# Create your tests here.
 
 
-# class GaleryTestCase(TestCase):
+class GaleryTestCase(TestCase):
 
-# def setUp(self):
-#     self.nairobi = Location(locatio='Nairobi')
-#     self.nairobi.save()
-#     self.nature = Category(name='nature')
-#     self.nature.save()
 
-#     self.waterfall = Image(
-#         name='waterfall', description='picture of a waterfall')
+def setUp(self):
+    self.nairobi = Location(locatio='Nairobi')
+    self.nairobi.save()
+    self.nature = Category(name='nature')
+    self.nature.save()
 
-#     self.waterfall.category.add(self.nature)
-#     self.waterfall.location.add(self.nairobi)
-#     self.waterfall.save()
+    self.waterfall = Image(
+        name='waterfall', description='picture of a waterfall')
+
+    self.waterfall.category.add(self.nature)
+    self.waterfall.location.add(self.nairobi)
+    self.waterfall.save()
