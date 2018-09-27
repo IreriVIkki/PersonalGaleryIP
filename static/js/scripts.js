bd0a595ea78bd61d8f50cd5da1da5bffd254b858
IreriVikki <wambsviki@gmail.com>
2018-09-27 20:53:37 +0300

    added a modal to display the current image and also a way to expand and minimise the image on click

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,19 @@
-$(document).ready(function () {
-    $('#click').click(function (e) {
-        e.preventDefault();
-        console.log('workd')
-    });
+function getId(id, value) {
+    idm = '#modal' + value
+    console.log(idm)
+    $(idm).removeClass('d-none');
+    $('.overlay').removeClass('d-none');
+}
+
+$('.overlay').click(function (e) {
+    e.preventDefault();
+    $('.one-image').addClass('d-none');
+    $(this).addClass('d-none');
+});
+
+$('.modal-img').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('modal-img-lg');
+    $(this).toggleClass('modal-img-sm');
+    // $(selector).toggleClass(className);
 });
